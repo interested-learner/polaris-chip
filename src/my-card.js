4b85f9e453e8c84a6332d19f1f7db615ec2c620f
interested-learner <ntcressler@gmail.com>
2026-02-13 10:19:45 -0500

    Added fancy feature and scroll feature

--- src/my-card.js
+++ src/my-card.js
@@ -20,7 +20,20 @@ export class MyCard extends LitElement {
     this.title = "Mount Fuji";
     this.paragraph = "Standing at 3,776 meters, Mt. Fuji is the tallest peak in Japan, the result of volcanic activity that began approximately 100,000 years ago.";
     this.buttonText = "Details";
+    this.link = "https://hax.psu.edu";
+    this.fancy = false;
   }
+
+  // put this anywhere on the MyCard class; just above render() is probably good
+openChanged(e) {
+  console.log(e);
+  if (e.target.getAttribute('open') !== null) {
+    this.fancy = true;
+  }
+  else {
+    this.fancy = false;
+  }
+}
 
   render() {
     return html`
@@ -29,15 +42,25 @@ export class MyCard extends LitElement {
           <img src="${this.image}" alt="${this.alt}">
         </div>
 
-        <h3 class="heading">${this.title}</h3>
+        <h3 class="heading">
+          <slot name="heading">${this.title}</slot>
+        </h3>
 
-        <p class="paragraph">${this.paragraph}</p>
+        <div class="paragraph">
+          <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+            <summary>Description</summary>
+            <div>
+              <slot>${this.paragraph}</slot>
+            </div>
+          </details>
+        </div>
 
-        <a href="https://hax.psu.edu">
-          <button class="button">${this.buttonText}</button>
+        <a href=${this.link}>
+          <button class="button">
+            <slot name="button">${this.buttonText}</slot>
+          </button>
         </a>
       </div>
-
     `;
   }
 
@@ -47,82 +70,115 @@ export class MyCard extends LitElement {
       image: { type: String },
       alt: { type: String },
       paragraph: { type: String },
-      buttonText: { type: String }
+      buttonText: { type: String },
+      link: { type: String },
+      fancy: { type: Boolean, reflect: true },
     };
   }
 
   static get styles() {
     return css`
       :host {
-        display: block;
+      display: inline-block;
       }
 
       .card {
-        width: 300px;
-        background-color: white;
-        border-radius: 16px;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
-        margin: 25px;
-        display: inline-block;
+      width: 300px;
+      background-color: white;
+      border-radius: 16px;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+      margin: 25px;
+      display: inline-block;
+      height: 320px;
+      overflow-y: scroll;
+      }
+
+      :host([fancy]) .card {
+      background-color: pink;
+      border: 2px solid fuchsia;
+      box-shadow: 10px 5px 5px red;
       }
       
       .image img {
-        width: 300px;
-        height: 200px;
-        border-radius: 8px;
+      width: 300px;
+      height: 200px;
+      border-radius: 8px;
       }
       
       .heading {
-        text-align: center;
-        color: var(--my-card-title-color, black);
+      text-align: center;
+      color: var(--my-card-title-color, black);
       }
       
       .paragraph {
-        font-size: 16px;
-        margin: 10px;
-        text-align: left;
-        color: var(--my-card-paragraph-color, black);
+      font-size: 16px;
+      margin: 10px;
+      text-align: left;
+      color: var(--my-card-paragraph-color, black);
       }
       
       .button {
-        display: none;
-        background-color: blue;
-        font-size: 18px;
-        border: none;
-        padding: 12px 10px;
-        text-align: center;
-        margin: 2px 10px 20px 30px;
-        width: 80%;
-        border-radius: 8px;
-        color: var(--my-card-button-text-color, black);
+      display: none;
+      background-color: blue;
+      font-size: 18px;
+      border: none;
+      padding: 12px 10px;
+      text-align: center;
+      margin: 2px 10px 20px 30px;
+      width: 80%;
+      border-radius: 8px;
+      color: var(--my-card-button-text-color, black);
+      }
+
+      details summary {
+    text-align: left;
+    font-size: 20px;
+    padding: 8px 0;
+  }
+
+  details[open] summary {
+    font-weight: bold;
+  }
+  
+  details div {
+    border: 2px solid black;
+    text-align: left;
+    padding: 8px;
+    height: 70px;
+    overflow: auto;
+  }
+
+      .details {
+        max-height: 100px;
+        overflow-y: scroll;
       }
       
       @media (min-width: 500px) and (max-width: 800px) {
-        .button {
-          display: block;
-        }
+      .button {
+        display: inline-block;
+      }
       }
       
       @media (max-width: 500px) {
-        .card {
-          width: 250px;
-        }
-        .image img {
-          width: 250px;
-          height: 150px;
-        }
-        .paragraph {
-          font-size: 14px;
-        }
+      .card {
+        width: 250px;
+      }
+      .image img {
+        width: 250px;
+        height: 150px;
+      }
+      .paragraph {
+        font-size: 14px;
+      }
       }
       
       .button:focus,
       .button:hover {
-        background-color: black;
+      background-color: black;
       }
       
       .bg-change {
-        background-color: pink;
+      background-color: pink;
       }
     `;
   }
